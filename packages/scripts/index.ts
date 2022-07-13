52f19df34fa79957ec4ad03b410212f049b0a174
Nathaniel Lin <linlilulll@gmail.com>
2022-07-13 21:25:49 +0800

    revert test change

--- packages/scripts/index.ts
+++ packages/scripts/index.ts
@@ -6,7 +6,6 @@ import { spinner } from 'zx/experimental';
 import { cleanBlobStorage } from './azure/clean-blob-storage.js';
 import { clearSecret } from './azure/clear-secret.js';
 import { readSecret } from './azure/read-secret.js';
-import { test } from './azure/test.js';
 import { uploadToBlobStorage } from './azure/upload-to-blob-storage.js';
 import { writeSecret } from './azure/write-secret.js';
 
@@ -31,10 +30,6 @@ const functionMap: Record<string, { message: string; func: Function }> = {
   'azure/clear-secret': {
     message: `clearing secrets from ${process.argv[3]}/${process.argv[4]}`,
     func: clearSecret,
-  },
-  test: {
-    message: 'test',
-    func: test,
   },
 };
 
